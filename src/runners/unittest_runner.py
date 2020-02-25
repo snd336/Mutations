6a34f1dca993ec5656ddb25f01c6338ab86d1b63
Steph <49566122+snd336@users.noreply.github.com>
2020-02-25 13:41:04 -0600

    add bitstring to source files

--- src/runners/unittest_runner.py
+++ src/runners/unittest_runner.py
@@ -1,16 +1,26 @@
 import unittest
+import os
+from src.tests.test__dictset import suite as dictset_suit
 
 
-def suites():
-    loader = unittest.TestLoader()
-    suite = unittest.TestSuite(loader.discover('./src/tests'))
-    return suite
+
+def debug_suit(src_name):
+    if src_name == 'dictset.py':
+        suite_obj = dictset_suit()
+        suite_obj.debug()
+    elif src_name == 'bitstring.py':
+        tests_path = 'C:\\Users\\steph\\Desktop\\Thesis\\Feature Scripts\\Working\\src\\tests\\bitstringtests'
+        loader = unittest.TestLoader()
+        suite_obj = unittest.TestSuite(loader.discover(tests_path))
+        suite_obj.debug()
 
 
-def debug_suit():
-    suite_obj = suites()
-    suite_obj.debug()
+def run_suit():
+    suite_obj = dictset_suit()
+    result = unittest.TestResult()
+    suite_obj.run(result)
+    print(result.testsRun)
 
 
 if __name__ == '__main__':
-    debug_suit()
+    debug_suit('bitstring.py')
