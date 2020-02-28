35d60e34b91b63ae5f69dd8476f2ad4951ed36a7
Steph <49566122+snd336@users.noreply.github.com>
2020-02-28 14:25:55 -0600

    update to allow for multiple source files at once

--- scripts/trace_script.py
+++ scripts/trace_script.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 from src.runners.unittest_runner import debug_suit
 
@@ -11,14 +10,15 @@ class LineObject:
 
 
 class Cover:
-    def __init__(self, src_name=None, test_name=None):
+    def __init__(self, prg_name=None, src_name=None, test_name=None):
+        self.prg_name = prg_name
         self.src_name = src_name
-        self.test_name = []
-        if type(test_name) == list:
-            self.test_name = test_name
-        else:
-            self.test_name.append(test_name)
-        self.counts = {}
+        self.test_name = test_name
+        self.src_counts = {}
+
+        for file in src_name:
+            self.src_counts[file] = {}
+
         self.current_test = None
         self.globaltrace = self.globaltrace_count
         self.localtrace = self.localtrace_count
@@ -27,7 +27,7 @@ class Cover:
         if why == "call":
             filename = frame.f_code.co_filename
             filename = filename.split('\\')[-1]
-            if filename == self.src_name:
+            if filename in self.src_name:
                 return self.localtrace
             if filename not in self.test_name:
                 return
@@ -50,18 +50,18 @@ class Cover:
         if why == "line":
             filename = frame.f_code.co_filename
             filename = filename.split('\\')[-1]
-            if filename != self.src_name:
+            if filename not in self.src_name:
                 return
-
+            current_dict = self.src_counts[filename]
             lineno = frame.f_lineno
             key = lineno
-            if self.counts.get(key) is None:
+            if current_dict.get(key) is None:
                 new_ctx = LineObject()
                 new_ctx.count = 1
                 new_ctx.ctx.append(self.current_test)
-                self.counts[key] = new_ctx
+                current_dict[key] = new_ctx
             else:
-                old_ctx = self.counts[key]
+                old_ctx = current_dict[key]
                 old_ctx.count += 1
                 if self.current_test not in old_ctx.ctx:
                     old_ctx.ctx.append(self.current_test)
@@ -70,18 +70,18 @@ class Cover:
 
     def run_trace(self):
         sys.settrace(self.globaltrace)
-        debug_suit(self.src_name)
+        debug_suit(self.prg_name)
         sys.settrace(None)
-        return self.counts
+        return self.src_counts
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    print("0--- %s seconds ---" % (time.time() - start_time))
-    cov = Cover(src_name='bitstring.py', test_name=['test_bitarray.py', 'test_bits.py', 'test_bitstore.py', 'test_bitstream.py', 'test_bitstring.py', 'test_constbitstream.py'])
+
+    cov = Cover(prg_name='astmonkey', src_name=['transformers.py'],
+                test_name=['test_transformers.py'])
     cov_dict = cov.run_trace()
-    print("1--- %s seconds ---" % (time.time() - start_time))
-    print(cov_dict.keys())
-    #for i in cov_dict:
-        #print(i, cov_dict[i].ctx)
+    for key in cov_dict['transformers.py'].keys():
+        print(key, cov_dict['transformers.py'][key].ctx)
+
+    
 
