60ef6fccb27bc616e5687d2e935aedfed08605d4
Steph <49566122+snd336@users.noreply.github.com>
2020-02-28 14:27:41 -0600

    add astmonkey and mutpy test runners

--- src/runners/unittest_runner.py
+++ src/runners/unittest_runner.py
@@ -1,26 +1,30 @@
 import unittest
-import os
-from src.tests.test__dictset import suite as dictset_suit
+import pytest
+import sys
+from src.tests.dictsettests.test__dictset import suite as dictset_suit
 
 
-
-def debug_suit(src_name):
-    if src_name == 'dictset.py':
+def debug_suit(prg_name):
+    if prg_name == 'dictset':
         suite_obj = dictset_suit()
         suite_obj.debug()
-    elif src_name == 'bitstring.py':
+    elif prg_name == 'bitstring':
         tests_path = 'C:\\Users\\steph\\Desktop\\Thesis\\Feature Scripts\\Working\\src\\tests\\bitstringtests'
         loader = unittest.TestLoader()
         suite_obj = unittest.TestSuite(loader.discover(tests_path))
         suite_obj.debug()
-
-
-def run_suit():
-    suite_obj = dictset_suit()
-    result = unittest.TestResult()
-    suite_obj.run(result)
-    print(result.testsRun)
-
+    elif prg_name == 'astmonkey':
+        old_stdout = sys.stdout
+        f = open('nul', 'w')
+        sys.stdout = f
+        pytest.main(['-x', 'C:\\Users\\steph\\Desktop\\Thesis\\Feature Scripts\\Working\\src\\tests\\astmonkeytests'])
+        sys.stdout = old_stdout
+    elif prg_name == 'mutpy':
+        old_stdout = sys.stdout
+        f = open('nul', 'w')
+        sys.stdout = f
+        pytest.main(['-x', 'C:\\Users\\steph\\Desktop\\Thesis\\Feature Scripts\\Working\\src\\tests\\mutpytests'])
+        sys.stdout = old_stdout
 
 if __name__ == '__main__':
-    debug_suit('bitstring.py')
+    debug_suit('bitstring')
